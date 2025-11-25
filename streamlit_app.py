3c4612d62cb8a68e104bd9c5e60fc7e787338764
reasnathaliereyn-crypto <reasnathaliereyn@gmail.com>
2025-11-25 15:42:31 +0800

    Update streamlit_app.py
    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if ingredients_list:
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
         #st.text(smoothiefroot_response.json())
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
