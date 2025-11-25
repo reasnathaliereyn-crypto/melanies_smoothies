f6e648336cb7a8422b5005d26a6c133f9c993544
reasnathaliereyn-crypto <reasnathaliereyn@gmail.com>
2025-11-25 10:41:07 +0800

    Update streamlit_app.py
    Call the SmoothieFroot API from Our SniS App!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -58,3 +59,6 @@ if  time_to_insert:
 #ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+ name_on_order + '!' , icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
