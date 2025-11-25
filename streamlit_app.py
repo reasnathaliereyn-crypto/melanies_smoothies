06d3939b61814797ffd79a3bdce632e2ac5b4833
reasnathaliereyn-crypto <reasnathaliereyn@gmail.com>
2025-11-25 11:02:48 +0800

    Update streamlit_app.py
    Get the SmoothieFroot Data to Show Nutrition Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -60,6 +63,4 @@ if  time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+ name_on_order + '!' , icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
